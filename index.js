4e9dd3832e472c84b0c888c3626c7716f0ee8cdb
hoangcaominh <hoangcaominh1812@gmail.com>
2023-09-27 15:14:58 +0700

    Update module

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { decrypt06, decrypt, unlzss } = require("./build/Release/thrpy-decode.node")
 
+const {
+    decrypt06,
+    decrypt,
+    unlzss
+} = thrpyDecode
+
 export {
+    thrpyDecode as default,
     decrypt06,
     decrypt,
     unlzss
