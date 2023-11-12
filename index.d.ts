f64c0636150c3ffa06684e91418acc8cf3fb7c7d
hoangcaominh <hoangcaominh1812@gmail.com>
2023-11-13 01:35:41 +0700

    Add type declaration

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export function decrypt06(buffer: Buffer, key: number, start = 0)
+export function decrypt(buffer: Buffer, blockSize: number, base: number, add: number)
+export function unlzss(buffer: Buffer)
